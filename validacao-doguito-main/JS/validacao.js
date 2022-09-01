06344c19ef90b22f80ecee6b24bd8dbfa8a12880
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-09-01 16:06:45 -0300

    Add files via upload

--- validacao-doguito-main/JS/validacao.js
+++ validacao-doguito-main/JS/validacao.js
@@ -1,15 +1,21 @@
-const dataNascimento = document.querySelector('#nascimento')
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-dataNascimento.addEventListener('blur' (evento) => {
-    validaDataNascimento(evento.target)
-})
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
     let mensagem = ''
 
    if (!maiorQue18(dataRecebida)){
-    mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    mensagem = 'Você deve ser maior de 18 anos para se cadastrar.'
    }
         input.setCustomValidity(mensagem)
 }

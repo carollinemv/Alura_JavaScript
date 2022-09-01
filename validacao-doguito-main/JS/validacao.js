53760bd34088b5c80c32683c337f37c6f4164bf5
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-09-01 17:09:27 -0300

    Add files via upload

--- validacao-doguito-main/JS/validacao.js
+++ validacao-doguito-main/JS/validacao.js
@@ -4,6 +4,30 @@ export function valida(input) {
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
+    if(input.validity.valid){
+       input.parentElement.classList.remove('input-container--invalido') 
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+    }
+}
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode está vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de emailnão pode está vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode está vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres,deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo da data não pode está vazio.',
+        customError: 'Você deve ser maior de 18 anos'
+    }
+    
 }
 
 const validadores = {

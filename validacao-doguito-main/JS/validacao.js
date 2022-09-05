a79611ebb10f71ecfa1aeaa6d21804cc671a14b9
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-09-05 16:57:06 -0300

    Add files via upload

--- validacao-doguito-main/JS/validacao.js
+++ validacao-doguito-main/JS/validacao.js
@@ -80,7 +80,7 @@ function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D/g, '')
     let mensagem = ''
 
-    if(!checaCPFRepetido(cpfFormatado)) {
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
         mensagem = 'O CPF digitado não é válido.'
     }
 
@@ -109,3 +109,43 @@ function checaCPFRepetido(cpf) {
 
     return cpfValido
 }
+function checaEstruturaCPF(cpf) {
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+    if(multiplicador >= 12){
+        return true
+    }
+    
+    
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('')
+    const digitoVerificdor = cpf.charAt(multiplicador - 1)
+    for(let contador = 0; multiplicadorInicial > 1 ; multiplicadorInicial--) {
+       soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+       contador++
+
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+    return false
+
+}
+
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11)
+}
+
+
+// 123 456 789 09
+
+// let soma = (11 * 1) + (10 *2) + (9 * 3) ... (2 * 0)
+
+// let digitoVerificador = 11 - (soma % 11)
